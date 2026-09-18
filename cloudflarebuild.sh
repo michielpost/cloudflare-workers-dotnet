bd2388f25bb530127d4b310f3b77730c7d17f1ec
Michiel Post <michiel@michielpost.nl>
2026-09-18 11:03:01 +0200

    adds dotnet to the path

--- cloudflarebuild.sh
+++ cloudflarebuild.sh
@@ -3,4 +3,8 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 
+# Add dotnet to PATH so MSBuild targets (e.g. Workers.targets) can invoke the `dotnet` command
+export PATH="$(pwd)/dotnet:$PATH"
+export DOTNET_ROOT="$(pwd)/dotnet"
+
 ./dotnet/dotnet publish src -c Release
