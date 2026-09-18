6c6f876147844db4354f03eba234cdd80bcecbb2
Michiel Post <michiel@michielpost.nl>
2026-09-18 10:56:33 +0200

    Added build script

---
+++ cloudflarebuild.sh
@@ -0,0 +1,6 @@
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+
+./dotnet/dotnet publish src -c Release
