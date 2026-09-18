09a8e12fd264d2f6f1930512d66ca900582713de
Michiel Post <michiel@michielpost.nl>
2026-09-18 10:48:52 +0200

    Hello world setup

---
+++ src/WorkersDotNet/Worker.cs
@@ -0,0 +1,19 @@
+﻿using Workers;
+
+namespace WorkersDotNet
+{
+    public static class Worker
+    {
+        [Fetch]
+        public static Task<Response> FetchAsync(
+            Request request,
+            Env environment,
+            Context context)
+        {
+            if (request.Path == "/")
+                return Task.FromResult(Response.Text("Hello from C# on Cloudflare Workers."));
+
+            return Task.FromResult(Response.Text("Not found", status: 404));
+        }
+    }
+}
